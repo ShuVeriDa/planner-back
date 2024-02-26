1114ad373ce30b2067956feb744131c74216f3d8
shuverida <bashtarov@outlook.com>
2024-02-26 14:48:12 +0300

    generated auth and user controller, module, service. And added jwt.strategy.ts, jwt.config.ts, also auth and user decorators

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { ConfigService } from '@nestjs/config'
+import { UserService } from '../user/user.service'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private configService: ConfigService, //нужен для того, что достать jwt токен из .env файла
+		private userService: UserService
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+			ignoreExpiration: true,
+			secretOrKey: configService.get("JWT_SECRET")
+		})
+	}
+
+	async validate({id}: {id: string}) {
+		return this.userService.getById(id)
+	}
+
+}
